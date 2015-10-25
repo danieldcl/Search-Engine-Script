c16894fb54cc70f5ce5101d6d8bec533f65e1c40
Dc Liao <dcliao92@gmail.com>
2015-10-24 23:57:56 -0400

    code in test.py

---
+++ test.py
@@ -0,0 +1,15 @@
+import urllib2
+from bs4 import BeautifulSoup
+
+
+webpage = urllib2.urlopen('http://www.ask.com/web?q=pineapple&qsrc=0&o=0&l=dir&qo=homepageSearchBox')
+webpage = urllib2.urlopen('https://www.google.com/?gws_rd=ssl#q=pineapple')
+
+
+soup= BeautifulSoup(webpage.read())
+links= soup.find_all('p')
+#description= soup.find_all('p', 'web-result-description')
+
+for i in range(10):
+	print links[i] ,'\n'
+	#print description[i].string , '\n'
